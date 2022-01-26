f951982f4672c9d9c0aa6f677cdd0a4abf76c881
HappyLandEric <98317763+HappyLandEric@users.noreply.github.com>
2022-01-26 16:38:21 +0700

    Update 16_landSale.js

--- deploy/16_landSale.js
+++ deploy/16_landSale.js
@@ -52,14 +52,21 @@ module.exports = async (hre) => {
   log('\n  Contract Deployment Data saved to "deployments" directory.')
 
   log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-  
+
   log('\n  LandSale set operator.')
   await landSale.setOperator('0x0b4d496fcdbcd5b1f696946276d61e13c441eca2')
   log('\n  LandSale set token payment.')
-  await landSale.addTokenAccept('0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF', true)
-  await landSale.addTokenAccept(hplAddress, true)
+  await landSale.updateMultiTokenAccept(['0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF', '0xe9e7cea3dedca5984780bafc599bd69add087d56', hplAddress], true)
   log('\n  LandSale set max Box.')
-  await landSale.setMaxBoxNumber(1000)
+  await landSale.setMaxBoxNumber(2000)
+  log('\n  LandSale set max tokenId.')
+  await landSale.setMaxLandId(41007)
+
+
+  const Land = await ethers.getContractFactory('Land')
+  const land = await Land.attach(landAddress)
+  log('setFactory Land...')
+  await land.setFactory(landSale.address)
 }
 
 module.exports.tags = ['landsale']
